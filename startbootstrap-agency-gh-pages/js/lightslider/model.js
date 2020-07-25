3bf4e23ba8380f78a6cc794a4e4d5e7a780b430b
iyangmaulanay <68388759+iyangmaulanay@users.noreply.github.com>
2020-07-25 22:56:06 +0700

    Add files via upload

---
+++ startbootstrap-agency-gh-pages/js/lightslider/model.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#vertical').lightSlider({
+        gallery: true,
+        item: 1,
+        vertical: true,
+        verticalHeight: 400,
+        vThumbWidth: 80,
+        thumbItem: 8,
+        thumbMargin: 4,
+        slideMargin: 0
+    });
+});
