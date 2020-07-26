b0a3a3a6188c1288627fe80882ed89972afeadbc
iyangmaulanay <68388759+iyangmaulanay@users.noreply.github.com>
2020-07-26 10:23:02 +0700

    Update model.js

--- startbootstrap-agency-gh-pages/js/lightslider/model.js
+++ startbootstrap-agency-gh-pages/js/lightslider/model.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
         gallery: true,
         item: 1,
         vertical: true,
-        verticalHeight: 400,
         vThumbWidth: 80,
+        verticalHeight: 500,
         thumbItem: 8,
         thumbMargin: 4,
-        slideMargin: 0
+        slideMargin: 0,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                verticalHeight: 400,
+                vThumbWidth: 70,
+                thumbItem: 8,
+            }
+        }]
     });
 });
