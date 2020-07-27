a8a0bf98179fecf8f6995a5288408336d6207231
iyangmaulanay <68388759+iyangmaulanay@users.noreply.github.com>
2020-07-27 15:24:30 +0700

    Update model.js

--- startbootstrap-agency-gh-pages/js/lightslider/model.js
+++ startbootstrap-agency-gh-pages/js/lightslider/model.js
@@ -1,10 +1,11 @@
+// model light slider
 $(document).ready(function () {
     $('#vertical').lightSlider({
         gallery: true,
         item: 1,
         vertical: true,
+        verticalHeight: 400,
         vThumbWidth: 80,
-        verticalHeight: 500,
         thumbItem: 8,
         thumbMargin: 4,
         slideMargin: 0,
